35399270110a11cc66e89853f2a3d3e3a9cfdfbd
wangsen <wangsen@ilongyuan.com.cn>
2017-11-23 00:36:31 +0800

    spark加载aws s3 数据至 Dframe 和 RDD，需配置hadoopConf，加载文件夹所有文件可行，加载文件前缀还需尝试

--- machinelearning-event-transform/src/main/scala/com/longyuan/machinelearning/event/transform/S3OtherEventDataToCSVDriver.scala
+++ machinelearning-event-transform/src/main/scala/com/longyuan/machinelearning/event/transform/S3OtherEventDataToCSVDriver.scala
@@ -1,6 +1,6 @@
 package com.longyuan.machinelearning.event.transform
 
-import org.apache.spark.SparkConf
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SparkSession
 
 /**
@@ -12,8 +12,8 @@ class S3OtherEventDataToCSVDriver {
 object S3OtherEventDataToCSVDriver {
   def apply: S3OtherEventDataToCSVDriver = new S3OtherEventDataToCSVDriver()
 
-  val accessKey = "AKIAOUYAW6RBUFR3ZF5A"
-  val secretKey = "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"
+  val accessKey = "AKIAO4RERNKGOMSUNZVA"
+  val secretKey = "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"
 
   val bucket = "csdatas3production"
   val appId = "bc4455c1f7a9c0f7"
@@ -21,10 +21,11 @@ object S3OtherEventDataToCSVDriver {
 
 
   def main(args: Array[String]): Unit = {
+    System.setProperty("hadoop.home.dir", "D:\\hadoop-2.6.5");
+
     val conf = new SparkConf()
-      .set("fs.s3a.endpoint", "s3.cn-north-1.amazonaws.com.cn")
-      .set("fs.s3a.access.key", accessKey)
-      .set("fs.s3a.secret.key", secretKey)
+        .setAppName("SparkStreamingWriteToParquetFileExample")
+        .setMaster("local[*]")
 
     val sqlContext = SparkSession
       .builder
@@ -32,13 +33,24 @@ object S3OtherEventDataToCSVDriver {
       .master("local[*]")
         .config(conf)
       .getOrCreate()
+    val sc = sqlContext.sparkContext
 
+//    val sc = new SparkContext(conf)
+    sc.hadoopConfiguration.set("fs.s3a.access.key", accessKey)
+    sc.hadoopConfiguration.set("fs.s3a.secret.key", secretKey)
+    sc.hadoopConfiguration.set("fs.s3a.endpoint", "s3.cn-north-1.amazonaws.com.cn")
 
 //   val otherDf = sqlContext.read.load(s"s3n://$accessKey:$secretKey@$bucket/$date/other_00:00:15_1948.log", "json")
 
-    val data = sqlContext.sparkContext.textFile(s"s3a://$bucket/$date/other_00:00:15_1948.log")
+//    val data = sc.textFile(s"s3a://$accessKey:$secretKey@$bucket/$appId/$date")
+//val data = sc.textFile(s"s3a://$bucket/$appId/$date/exit*")
+//
+//    val data = sc.textFile("s3a://csdatas3production/234/wangsen")
+//    println(s"count: ${data.count}")
 
-    print(s"count: ${data.count}")
+    val otherDf = sqlContext.read.json("s3a://csdatas3production/234/wangsen")
+    otherDf.printSchema()
+    otherDf.show(10)
 
   }
 }
